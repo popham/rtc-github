4584262744c84365abb2a2e4d5f770e9fe4eb254
Tim Popham <popham@uw.edu>
2014-10-04 23:03:35 -0700

    Initial

---
+++ example/messages/capnp/client.capnp.d/rTypes.js
@@ -0,0 +1,5 @@
+define(['capnp-js/reader/index'], function(reader) {
+    var types = {};
+    types["0xd25f3f5837562913"] = reader.structure(6, 0, 8);
+    return types;
+});
---
+++ example/messages/capnp/server.capnp
@@ -0,0 +1,5 @@
+@0xc6c0baeb8ceae645;
+
+struct Server {
+    message @0 :List(Text);
+}
