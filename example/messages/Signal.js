b3ee8e2471b50575bb58c456b5fa8163d0ed9f60
Tim Popham <popham@uw.edu>
2014-10-06 13:00:04 -0700

    Migrate to new protocol

--- example/messages/Signal.js
+++ example/messages/Signal.js
@@ -3,47 +3,67 @@ define(['cookies', 'js-signals', 'capnp-js/stream', 'capnp-js/builder/Allocator'
 
     var allocator = new Allocator();
 
-    var Connection = function () {
-        this.onResponded = new signals.Signal();
-        this.onClosed = new signals.Signal();
+    var Signal = function () {
+        this.messaged = new signals.Signal();
+        this.closed = new signals.Signal();
         this._socket = null;
 
         this._reconnect();
 
         this.peer = {
-            offer : function (uid) {
+            offer : function (uid, sdp) {
                 var request = allocator.initRoot(client.Client);
-                request.initPeer();
-                request.getPeer().getOffer().setTarget(uid);
+
+                request.getTarget().setUserId(uid);
+
+                var peer = request.initPeer();
+                var offer = peer.initOffer();
+                offer.setSdp(sdp);
+
                 this._send(request);
             }.bind(this),
-            answer : function (uid) {
+            answer : function (uid, sdp) {
                 var request = allocator.initRoot(client.Client);
-                request.initPeer();
-                request.getPeer().getAnswer().setTarget(uid);
+
+                request.getTarget().setUserId(uid);
+
+                var peer = request.initPeer();
+                var answer = peer.initAnswer();
+                answer.setSdp(sdp);
+
+                this._send(request);
+            }.bind(this),
+            iceCandidate : function (uid, candidate) {
+                var request = allocator.initRoot(client.Client);
+
+                request.getTarget().setUserId(uid);
+
+                var peer = request.initPeer();
+                peer.setIceCandidate(candidate);
+
                 this._send(request);
             }.bind(this)
         };
     };
 
-    Connection.prototype.service = function (offer) {
+    Signal.prototype.service = function (offer) {
         var request = allocator.initRoot(client.Client);
         request.initService();
         request.getService().setOffer(offer);
         this._send(request);
     };
 
-    Connection.prototype._reconnect = function () {
+    Signal.prototype._reconnect = function () {
         var socket = this._socket = new WebSocket(settings.socketServer);
 
         socket.onmessage = function (e) {
             var response = stream.toArena(e.data).getRoot(server.Server);
-            this.onResponded.dispatch(response);
+            this.messaged.dispatch(response);
         }.bind(this);
 
         socket.onclose = function (e) {
             this._reconnect();
-            this.onClosed.dispatch(e.wasClean, e.code, e.reason);
+            this.closed.dispatch(e.wasClean, e.code, e.reason);
         }.bind(this);
 
         socket.onopen = function () {
@@ -57,11 +77,11 @@ define(['cookies', 'js-signals', 'capnp-js/stream', 'capnp-js/builder/Allocator'
         }.bind(this);
     };
 
-    Connection.prototype._send = function (request) {
+    Signal.prototype._send = function (request) {
         stream.fromStruct(request).forEach(function (segment) {
             this._socket.send(segment);
         }.bind(this));
     };
 
-    return Connection;
+    return Signal;
 });
