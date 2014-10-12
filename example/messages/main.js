c9aa13b525870f25b87c807613a41a1b0ba69d66
Tim Popham <popham@uw.edu>
2014-10-12 10:35:20 -0700

    Interact with DOM on ready, and migrate to oauth terminal with loggedIn hash

--- example/messages/main.js
+++ example/messages/main.js
@@ -1,143 +1,166 @@
-define(['./StateMachine', './Service', './Client', './Signal'], function (
-           StateMachine,     Service,     Client,     Signal) {
+define(['domReady', './StateMachine', './Service', './Client', './Signal'], function (
+         domReady,     StateMachine,     Service,     Client,     Signal) {
 
-    // Control
-    var hosts = document.getElementById('hosts');
-    var offer = document.getElementById('offer');
-    var accept = document.getElementById('accept');
-    var quit = document.getElementById('quit');
+    domReady(function () {
+        // Control
+        var hosts = document.getElementById('hosts');
+        var offer = document.getElementById('offer');
+        var accept = document.getElementById('accept');
+        var quit = document.getElementById('quit');
 
-    // Messages
-    var history = document.getElementById('history');
-    var message = document.getElementById('message');
-    var send = document.getElementById('send');
-    var clear = document.getElementById('clear');
+        // Messages
+        var history = document.getElementById('history');
+        var message = document.getElementById('message');
+        var send = document.getElementById('send');
+        var clear = document.getElementById('clear');
 
-    var selectedHost = function () {
-        var i = hosts.selectedIndex;
-        return hosts.item(i).value;
-    };
+        var selectedHost = function () {
+            var i = hosts.selectedIndex;
+            return hosts.item(i).value;
+        };
 
-    var onSend = function (client) {
-        return function (event) {
-            var m = message.value.trim();
+        var onSend = function (client) {
+            return function (event) {
+                var m = message.value.trim();
+                if (m !== '') {
+                    client.send(m);
+                    var p = document.createElement('p');
+                    p.innerHTML = m;
+                    history.appendChild(p);
+                }
+
+                return false;
+            };
+        };
+
+        var onClear = function (event) {
+            if (message.value !== '') {
+                message.value = '';
+            }
+
+            return false;
+        };
+
+        var onMessage = function (message) {
+            var name = message.getSource().getName().asString();
+            var m = message.getValue().asString().trim();
             if (m !== '') {
-                client.send(m);
                 var p = document.createElement('p');
-                p.innerHTML = m;
+                p.innerHTML = '<p><span>'+name+'</span><br>'+m+'</p>';
                 history.appendChild(p);
             }
 
             return false;
         };
-    };
 
-    var onClear = function (event) {
-        if (message.value !== '') {
-            message.value = '';
+        var uiReset = function () {
+            hosts.disabled = false;
+            offer.disabled = false;
+            accept.disabled = false;
+            quit.disabled = false;
+            history.disabled = false;
+            message.disabled = false;
+            send.disabled = false;
+            clear.disabled = false;
+        };
+        var uiFree = function () {
+            uiReset();
+            quit.disabled = true;
+            message.disabled = true;
+            send.disabled = true;
+            clear.disabled = true;
+            message.value = "";
+        };
+        var uiHost = function () {
+            uiReset();
+            hosts.disabled = true;
+            offer.disabled = true;
+            accept.disabled = true;
+        };
+        var uiGuest = function () {
+            uiReset();
+            hosts.disabled = true;
+            offer.disabled = true;
+            accept.disabled = true;
+        };
+
+        var signal = null;
+        var client = null;
+        var service = null;
+
+        var logout = [function () {
+            signal.kill();
+            signal = null;
+        }, 'anonymous'];
+
+        var current;
+        switch (document.URL.hash) {
+        case 'loggedIn': current = 'authenticated'; break;
+        case 'loggedOut': current = 'anonymous'; break;
+        default: throw new Error('Unrecognized hash value');
         }
 
-        return false;
-    };
-
-    var onMessage = function (message) {
-        var name = message.getSource().getName().asString();
-        var m = message.getValue().asString().trim();
-        if (m !== '') {
-            var p = document.createElement('p');
-            p.innerHTML = '<p><span>'+name+'</span><br>'+m+'</p>';
-            history.appendChild(p);
-        }
-
-        return false;
-    };
-
-    var uiReset = function () {
-        hosts.disabled = false;
-        offer.disabled = false;
-        accept.disabled = false;
-        quit.disabled = false;
-        history.disabled = false;
-        message.disabled = false;
-        send.disabled = false;
-        clear.disabled = false;
-    };
-    var uiFree = function () {
-        uiReset();
-        quit.disabled = true;
-        message.disabled = true;
-        send.disabled = true;
-        clear.disabled = true;
-        message.value = "";
-    };
-    var uiHost = function () {
-        uiReset();
-        hosts.disabled = true;
-        offer.disabled = true;
-        accept.disabled = true;
-    };
-    var uiGuest = function () {
-        uiReset();
-        hosts.disabled = true;
-        offer.disabled = true;
-        accept.disabled = true;
-    };
-
-    var signal = new Signal();
-    signal.hostsUpdated.add(function (users) {
-        var options = '';
-        users.forEach(function (user) {
-            options += '<option value="'+user.getUid()+'">';
-            options += user.getName().asString();
-            options += '</option>';
-        });
-        hosts.innerHTML = options;
-    });
-
-    var client = null;
-    var service = null;
-    var state = new StateMachine(
-        'free',
-        {
-            free : {
-                host : [function () {
-                    uiHost();
-                    service = new Service(signal);
-                    var local = service.createLocalClient();
-                    local.messaged.add(onMessage);
-                    send.onclick = onSend(local);
-                    clear.onclick = onClear;
-                }, 'host'],
-                join : [function () {
-                    uiGuest();
-                    client = new Client(selectedHost(), signal);
-                    client.messaged.add(onMessage);
-                    send.onclick = onSend(client);
-                    clear.onclick = onClear;
-                }, 'guest']
-            },
-            host : {
-                quit : [function () {
-                    uiFree();
-                    service.kill();
-                    service = null;
-                    send.onclick = null;
-                    clear.onclick = null;
-                }, 'free']
-            },
-            guest : {
-                quit : [function () {
-                    uiFree();
-                    client.kill();
-                    client = null;
-                    send.onclick = null;
-                    clear.onclick = null;
-                }, 'free']
+        var state = new StateMachine(
+            'anonymous',
+            {
+                anonymous : {
+                    authenticate : [function () {
+                        signal = new Signal();
+                        signal.hostsUpdated.add(function (users) {
+                            var options = '';
+                            users.forEach(function (user) {
+                                options += '<option value="'+user.getUid()+'">';
+                                options += user.getName().asString();
+                                options += '</option>';
+                            });
+                            hosts.innerHTML = options;
+                        });
+                    }, 'authenticated']
+                },
+                authenticated : {
+                    logout : logout,
+                    offer : [function () {
+                        uiHost();
+                        service = new Service(signal);
+                        var local = service.createLocalClient();
+                        local.messaged.add(onMessage);
+                        send.onclick = onSend(local);
+                        clear.onclick = onClear;
+                    }, 'host'],
+                    accept : [function () {
+                        uiGuest();
+                        client = new Client(selectedHost(), signal);
+                        client.messaged.add(onMessage);
+                        send.onclick = onSend(client);
+                        clear.onclick = onClear;
+                    }, 'guest']
+                },
+                host : {
+                    logout : logout,
+                    quit : [function () {
+                        uiFree();
+                        service.kill();
+                        service = null;
+                        send.onclick = null;
+                        clear.onclick = null;
+                    }, 'authenticated']
+                },
+                guest : {
+                    logout : logout,
+                    quit : [function () {
+                        uiFree();
+                        client.kill();
+                        client = null;
+                        send.onclick = null;
+                        clear.onclick = null;
+                    }, 'authenticated']
+                }
             }
-        }
-    );
+        );
 
-    offer.onclick = function () { state.trigger('host'); };
-    accept.onclick = function () { state.trigger('guest'); };
-    quit.onclick = function () { state.trigger('free'); };
+        state.trigger(current);
+        offer.onclick = function () { state.trigger('offer'); };
+        accept.onclick = function () { state.trigger('accept'); };
+        quit.onclick = function () { state.trigger('quit'); };
+    });
 });
