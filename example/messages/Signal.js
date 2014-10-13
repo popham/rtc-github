1b3790957099722560bfaf647000b90d3eddc4d6
Tim Popham <popham@uw.edu>
2014-10-13 16:00:23 -0700

    Migrate session message to promises interface

--- example/messages/Signal.js
+++ example/messages/Signal.js
@@ -1,10 +1,22 @@
-define(['js-signals', 'capnp-js/packet', 'capnp-js/builder/Allocator', 'rtc-github-protocol/server.capnp.d/readers', 'rtc-github-protocol/client.capnp.d/builders', './settings'], function (
-            signals,            packet,                    Allocator,                       server,                                       client,                      settings) {
+define(['when', 'js-signals', 'capnp-js/packet', 'capnp-js/builder/Allocator', 'rtc-github-protocol/server.capnp.d/readers', 'rtc-github-protocol/client.capnp.d/builders', './settings'], function (
+         when,      signals,            packet,                    Allocator,                       server,                                       client,                      settings) {
 
     var allocator = new Allocator();
 
-    var Signal = function () {
-        this.sessioned = new signals.Signal();
+    var Signal = function (timeout) {
+        timeout = timeout || 5000;
+
+        // The session resolves on receipt of an introductory session.
+        this._deferredConnection = when.defer();
+        this.connecting = this._deferredConnection.promise;
+        setTimeout(
+            this._deferredConnection.reject,
+            timeout,
+            ['Signalling connection timeout']
+        );
+
+        this.hosts = Signal.EMPTY_HOSTS;
+
         this.hostsUpdated = new signals.Signal();
         this.peered = new signals.Signal();
         this.closed = new signals.Signal();
@@ -40,32 +52,44 @@ define(['js-signals', 'capnp-js/packet', 'capnp-js/builder/Allocator', 'rtc-gith
         };
     };
 
-    Signal.prototype.service = function (offer) {
+    Signal.EMPTY_HOSTS = allocator.initRoot(server.Server).initHostsUpdate(0).asReader();
+
+    Signal.prototype.service = function (isOffering) {
         var request = allocator.initRoot(client.Client);
         request.initService();
-        request.getService().setOffer(offer);
+        request.getService().setOffer(isOffering);
         this._send(request);
     };
 
     Signal.prototype.kill = function () {
+        this.connecting = null;
+        this.hosts = null;
+
         this.hostsUpdated.removeAll();
+        this.peered.removeAll();
+        this.closed.removeAll();
+
         this._socket.onclose = null;
         this._socket.close();
+        this._socket = null;
+
+        this._deferredConnection.reject('Signal killed');
     };
 
     Signal.prototype._reconnect = function () {
         var socket = this._socket = new WebSocket(settings.socketServer);
-
         socket.binaryType = 'arraybuffer';
 
         socket.onmessage = function (e) {
             var message = packet.toArena(new Uint8Array(e.data)).getRoot(server.Server);
+            var s;
             switch (message.which()) {
             case message.SESSION:
-                this.sessioned.dispatch(message.getSession());
+                this._deferredConnection.resolve(message.getSession());
                 break;
             case message.HOSTS_UPDATE:
-                this.hostsUpdated.dispatch(message.getHostsUpdate());
+                this.hosts = message.getHostsUpdate();
+                this.hostsUpdated.dispatch(this.hosts);
                 break;
             case message.PEER:
                 this.peered.dispatch(message.getPeer());
@@ -74,8 +98,16 @@ define(['js-signals', 'capnp-js/packet', 'capnp-js/builder/Allocator', 'rtc-gith
         }.bind(this);
 
         socket.onclose = function (e) {
-            this._reconnect();
+            this._deferredConnection.reject();
+            this._deferredConnection = when.defer();
+            this.connecting = this._deferredConnection.promise;
+
             this.closed.dispatch(e.wasClean, e.code, e.reason);
+
+            this.hosts = Signal.EMPTY_HOSTS;
+            this.hostsUpdated.dispatch(this.hosts);
+
+            this._reconnect();
         }.bind(this);
     };
 
