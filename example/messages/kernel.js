0916f7b5a08c881d7f3eb905e0f103957009932e
Tim Popham <popham@uw.edu>
2014-10-13 17:30:23 -0700

    New requirejs baseUrl

--- example/messages/kernel.js
+++ example/messages/kernel.js
@@ -1,11 +1,14 @@
 importScripts('resources/requirejs/require.js');
 requirejs.config({
-    baseUrl : 'resources',
-    paths : {}
+    baseUrl : '.',
+    paths : {
+        'capnp-js' : 'resources/capnp-js',
+        'capnp' : 'capnp'
+    }
 });
 
-requirejs(['capnp-js/packet', 'capnp-js/builder/Allocator', './capnp/client.capnp.d/readers', './capnp/server.capnp.d/builders'], function (
-                     packet,                    Allocator,           client,                           server) {
+requirejs(['capnp-js/packet', 'capnp-js/builder/Allocator', 'capnp/client.capnp.d/readers', 'capnp/server.capnp.d/builders'], function (
+                     packet,                    Allocator,         client,                         server) {
 
     var allocator = new Allocator();
 
