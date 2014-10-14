b6f8e23543116231fc955fc771f140aaba53a2d3
Tim Popham <popham@uw.edu>
2014-10-14 13:26:09 -0700

    Tracer

--- example/messages/kernel.js
+++ example/messages/kernel.js
@@ -20,10 +20,11 @@ requirejs(['capnp-js/packet', 'capnp-js/builder/Allocator', 'capnp/client.capnp.
         var root = allocator.initRoot(server.Server);
         var m = root.initMessages(1).get(0);
         console.log('WORKER');
-        console.log(message._dataSection);
-        console.log(message._pointersSection);
-        console.log(message._end);
-        console.log(message.getSource().which());
+        var s = message.getSource();
+        console.log(s._dataSection);
+        console.log(s._pointersSection);
+        console.log(s._end);
+        console.log(s.which());
         m.setSource(message.getSource().getUser());
         m.setValue(message.getValue());
         postMessage(packet.fromStruct(m));
