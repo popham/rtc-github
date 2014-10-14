68317675ca2ee0dec2d1edba6839572e84a629a5
Tim Popham <popham@uw.edu>
2014-10-13 19:30:37 -0700

    Tracer

--- example/messages/resources/capnp-js/builder/list/deref.js
+++ example/messages/resources/capnp-js/builder/list/deref.js
@@ -1,6 +1,8 @@
 define([ "../../reader/layout/list" ], function(layout) {
     return function(List) {
         return function(arena, pointer) {
+console.log('LAYOUT');
+console.log(layout.unsafe(arena, pointer));
             return new List(arena, layout.unsafe(arena, pointer), false);
         };
     };
