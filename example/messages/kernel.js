45879e60a2fabdf30069d3f19e660a733e2f25ff
Tim Popham <popham@uw.edu>
2014-10-13 18:43:10 -0700

    Fix reference error

--- example/messages/kernel.js
+++ example/messages/kernel.js
@@ -13,7 +13,7 @@ requirejs(['capnp-js/packet', 'capnp-js/builder/Allocator', 'capnp/client.capnp.
 
     onmessage = function (e) {
         var message = packet.toArena(e.data).getRoot(client.Client);
-        var root = allocator.init(server.Server);
+        var root = allocator.initRoot(server.Server);
         var m = root.initMessages(1).get(0);
         m.setSource(message.getSource().getUser());
         m.setValue(message.getValue());
