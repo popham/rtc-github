54b4a9479fc367258247a225bf355056c7fe602c
Tim Popham <popham@uw.edu>
2014-10-17 21:01:46 -0700

    Convert enumerants to string names

--- example/messages/toCandidate.js
+++ example/messages/toCandidate.js
@@ -1,9 +1,16 @@
 define([], function () {
 
     return function (ice) {
+        var mid;
+        switch (ice.getSdpMId()) {
+        case peer.MediaIdentifier.DATA: mid = 'data'; break;
+        case peer.MediaIdentifier.AUDIO: mid = 'audio'; break;
+        case peer.MediaIdentifier.VIDEO: mid = 'video'; break;
+        }
+
         return new RTCIceCandidate({
             candidate : ice.getCandidate().asString(),
-            sdpMid : ice.getSdpMId(),
+            sdpMid : mid,
             sdpMLineIndex : ice.getSdpMLineIndex()
         });
     };
