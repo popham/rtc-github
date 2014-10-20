9fdc928b8bb01709e4a06bda56979cea27bedaff
Tim Popham <popham@uw.edu>
2014-10-19 20:14:26 -0700

    Update

--- example/messages/resources/capnp-js/stream.js
+++ example/messages/resources/capnp-js/stream.js
@@ -1,46 +1,49 @@
 define([ "./reader/Arena", "./reader/primitives", "./builder/primitives" ], function(Arena, reader, builder) {
+    /*
+     * Implement stream serialization from Capnproto reference implementation.
+     *
+     * * instance Structure - Structure to derive chunks from.
+     *
+     * RETURNS: Array - The array's first element is the stream header.  The
+     * remaining elements are arena segments.
+     */
     var fromStruct = function(instance) {
         var arena = instance._arena;
-        var count = arena._segments.length - 1;
-        var segments = arena._segments.map(function(segment) {
-            var s = segment.subarray(0, segment._position);
-            s._id = segment._id;
-            s._position = segment._position;
-            return s;
-        });
+        var count = arena._segments.length;
         /*
          * Header Length
-         * count==0 => length==1 => 1 word
-         * count==1 => length==2 => 1.5 -> 2 word
-         * count==2 => length==3 => 2 word
+         * count==1 => 1 word
+         * count==2 => 1.5 -> 2 word
+         * count==3 => 2 word
          */
-        var bytes = new Uint8Array((count & 65534) + 1 << 3);
-        builder.uint32(count, bytes, 0);
-        for (var i = 0; i < arena._segments.length; ++i) {
-            builder.uint32(arena._segments[i]._position >>> 3, bytes, 1 + i << 2);
-        }
-        segments.unshift(bytes);
+        var headerLength = (count + 2 & 65534) << 2;
+        var header = new Uint8Array(headerLength);
+        builder.uint32(count - 1, header, 0);
+        var segments = arena._segments.map(function(segment, i) {
+            builder.uint32(segment._position, header, 1 + i << 2);
+            return segment.subarray(0, segment._position);
+        });
+        segments.unshift(header);
         return segments;
     };
-    var toArena = function(blob) {
-        if (blob.length < 4) {
+    /*
+     * Deserialize a stream to an Arena.
+     *
+     * * headerPlusSegments Array - See return of `fromStruct`.
+     *
+     * RETURNS: ReaderArena
+     */
+    var toArena = function(headerPlusSegments) {
+        if (headerPlusSegments.length < 2) {
             throw new RangeError();
         }
-        var segments = [];
-        var end = 4 + (reader.uint32(blob, 0) + 1 << 2);
-        if (blob.length < end) {
-            throw new RangeError();
+        var header = headerPlusSegments[0];
+        for (var i = 1; i < headerPlusSegments.length; ++i) {
+            var segment = headerPlusSegments[i];
+            segment[i]._id = i - 1;
+            segment[i]._position = segment.length;
         }
-        var begin = end;
-        for (var i = 0; i < end; i += 4) {
-            var length = reader.uint32(blob, i);
-            var s = blob.subarray(begin, begin + length);
-            s._id = i;
-            s._position = length;
-            segments.push(s);
-            begin += length;
-        }
-        return new Arena(segments);
+        return new Arena(headerPlusSegments.slice(1));
     };
     return {
         fromStruct: fromStruct,
